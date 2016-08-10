ebd435f762304ffcbe88aba8a10183e28ca502fb
Charnae Jackson <charnaejackson1018@gmail.com>
2016-08-10 19:08:46 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var mapCanvas = document.getElementById("map");
+var mapOptions = {
+    center: new google.maps.LatLng(42.331427,-83.045754), zoom: 10
+}
+var map = new google.maps.Map(mapCanvas, mapOptions);
